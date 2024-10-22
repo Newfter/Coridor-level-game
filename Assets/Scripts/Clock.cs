b4904a26dc07f78bcc5ae62e679f57fd267a2bfd
Newfter <153613256+Newfter@users.noreply.github.com>
2024-10-22 13:46:03 +0300

    c
    c2

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI clockText;
+    private int seconds, minutes, time;
+    private void Start() { StartCoroutine(ClockVoid()); }
+    private IEnumerator ClockVoid()
+    {
+        yield return new WaitForSeconds(1);
+        seconds += 1;
+        clockText.text = "" + seconds / 60 + " : " + seconds % 60;
+        StartCoroutine(ClockVoid());
+    }
+
+}
